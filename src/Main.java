cd40e5f7a13959a826ca66e0b3ef1fd211ada756
arianahir <arianahir123@gmail.com>
2025-05-24 20:53:30 +0600

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int variableOne = 23;
+        float variableTwo = 1.53f;
+        double variableThree = 1.74745467;
+        char variableFour = 'P';
+        String variableFive = "Hello World";
+
+        System.out.println(variableOne);
     }
 }
