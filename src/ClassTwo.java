ec22ce1773ba851aa1f4b5432ecaa4b70800d3da
arianahir <arianahir123@gmail.com>
2025-07-02 21:11:31 +0600

    Filled Pyramid

--- src/ClassTwo.java
+++ src/ClassTwo.java
@@ -1,11 +1,10 @@
 public class ClassTwo {
     public static void main(String[] args) {
-        int year = 1600;
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println("Leap Year");
-        } else {
-            System.out.println("Not leap year");
+        for(int a=1; a<=5; a++){
+            for(int b=1; b<=a; b++){
+                System.out.print("*");}
+            System.out.print("\n");}
+
         }
     }
-}
 
